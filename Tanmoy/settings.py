cb74411c15e2c572da8f3f778ab219a44553a7fe
Aman Bhar <amanbhar.aus@gmail.com>
2025-07-13 03:40:09 +0530

    Update settings.py

--- Tanmoy/settings.py
+++ Tanmoy/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v5owrvb_$6k2py^mlkv19%ptic5mp5e4pp_6)@7w*hnn80nyij"
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'insecure-dev-key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+
 
 
 # Application definition
